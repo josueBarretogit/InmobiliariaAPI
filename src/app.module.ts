2164811f8c7b258b4f2ab7d7653e195ce2e5c06f
josueBarretogit <josuebarretogit@gmail.com>
2023-09-26 10:56:26 -0500

    implementacion inmueble controllers,service y entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InmueblesModule } from './inmuebles/inmuebles.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Inmueble } from './inmuebles/entities/inmueble.entity';
 
 @Module({
-  imports: [],
+  imports: [InmueblesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'inmobiliariaapi',
+      entities: [Inmueble],
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
